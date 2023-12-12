990128795b426f398c27400c70d29c6206ab52c7
reserver7 <reserver7@gmail.com>
2023-12-12 17:00:07 +0900

    firebase 연동

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlloPJh3UwB2Q2tODeO-jvrI6FwvPF424",
+  authDomain: "react-disney-plus-app-a0fa8.firebaseapp.com",
+  projectId: "react-disney-plus-app-a0fa8",
+  storageBucket: "react-disney-plus-app-a0fa8.appspot.com",
+  messagingSenderId: "975555169643",
+  appId: "1:975555169643:web:8b251d65c522c81191102e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,7 +1,108 @@
 import React from "react";
+import { styled } from "styled-components";
 
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Content>
+        <Center>
+          <LogoOne src="/images/cta-logo-one.svg" />
+          <SignUpLink>GET ALL THERE</SignUpLink>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription <br></br> As of 03/26/21, the price of
+            Disney+ and The Disney Bundle will increase by $1
+          </Description>
+          <LogoTwo src="/images/cta-logo-two.png" />
+        </Center>
+      </Content>
+    </Container>
+  );
 };
 
 export default LoginPage;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-image: url("/images/login-background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const Center = styled.div`
+  max-width: 650px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const SignUpLink = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.p`
+  font-size: 11px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const LogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+  margin: 0 auto;
+`;

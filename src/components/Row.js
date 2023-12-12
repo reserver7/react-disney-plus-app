78b879622b6319dc1ba598317e3fe9a06deffd7a
reserver7 <reserver7@gmail.com>
2023-12-12 15:24:17 +0900

    custom hooks 생성

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useCallback } from "react";
 import axios from "../api/axios";
 import "./Row.css";
-import { styled } from "styled-components";
 import MovieModal from "./MovieModal/index";
 
 const Row = ({ title, id, fetchUrl }) => {
@@ -21,7 +20,6 @@ const Row = ({ title, id, fetchUrl }) => {
   const handleClick = (movie) => {
     setModalOpen(true);
     setMovieSelected(movie);
-    console.log("?>", movie);
   };
 
   return (
@@ -69,41 +67,3 @@ const Row = ({ title, id, fetchUrl }) => {
 };
 
 export default Row;
-
-const Container = styled.div`
-  padding: 0 0 26px;
-`;
-
-const Content = styled.div``;
-
-const Wrap = styled.div`
-  width: 95%;
-  height: 95%;
-  padding-top: 56.25%;
-  border-radius: 10px;
-  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
-    rgb(0 0 0/73%) 0px 16px 10px -10px;
-  cursor: pointer;
-  overflow: hidden;
-  position: relative;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-  border: 3px solid rgba(249, 249, 249, 0.1);
-
-  img {
-    inset: 0px;
-    display: block;
-    height: 100%;
-    object-fit: cover;
-    opacity: 1;
-    position: absolute;
-    width: 100%;
-    transition: opacity 500ms ease-in-out;
-    z-index: 1;
-  }
-  &:hover {
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-    transform: scale(0.98);
-    border-color: rgba(249, 249, 249, 0.8);
-  }
-`;
